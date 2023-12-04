212410200c9b2fec3cd2cc2fcee56495fc8d1fb0
Kevin-HESSE <kev.hesse@gmail.com>
2023-12-04 12:22:22 +0100

    testing the pathHelper

---
+++ src/helpers/__test__/pathHelper.test.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import { pathHelpers } from '../pathHelper';
+
+jest.mock('fs');
+
+describe('Test the method of pathHelpers', () => {
+  it('return the directory', () => {
+    jest.mocked(fs.existsSync)
+      .mockImplementationOnce(() => true)
+      .mockImplementationOnce(() => true)
+
+    const directory = pathHelpers.getDirectory('routers');
+
+    expect(directory).toBe('./src/routers');
+  })
+
+  it('throws an error when the directory does not exist', () => {
+    jest.mocked(fs.existsSync)
+      .mockImplementationOnce(() => true)
+      .mockImplementationOnce(() => false)
+
+    expect(() => {
+      pathHelpers.getDirectory('routers');
+    }).toThrow('The routers directory does not exist !');
+  })
+})
